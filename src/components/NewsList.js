6530b55a52dded755ccbec7cf82ea0b73920911e
jgam <jgam@alumni.nd.edu>
2020-03-23 17:49:37 +0900

    custom hooks

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,55 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
-const NewsListBlock = styled.div``;
+const NewsListBlock = styled.div`
+
+`;
 
 const NewsList = ({ category }) => {
-  console.log('rerendering');
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    //async function declaration specifically here
-    const fetchData = async () => {
-      setLoading(true);
-      console.log('fetchdata');
-      try {
-        //some axios call
-        //here u define categories from parent component that is received from
-        const query = category === 'all' ? '' : `&category=${category}`;
-        console.log('query is ');
-        console.log(query);
-        const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f' +
-            query,
-        );
-        console.log('even slower than state changes?');
-        console.log(response.data.articles);
-        setArticles(response.data.articles);
-        console.log('in try');
-      } catch (e) {
-        console.log(e);
-      }
-      console.log('between in try and rerendering at the end');
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`,
+    );
   }, [category]);
 
-  //when loading
+  // 대기 중일 때
   if (loading) {
-    console.log('here is loading');
-    return <NewsListBlock>loading..</NewsListBlock>;
+    return <NewsListBlock>대기 중...</NewsListBlock>;
   }
 
-  //when articles value not defined
-  if (!articles) {
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
 
-  //if articles
+  // response 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map(article => (
