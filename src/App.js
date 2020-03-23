66b822bd3c82f542a69de1a65e07422120571560
jgam <jgam@alumni.nd.edu>
2020-03-23 16:26:45 +0900

    Router with api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 import NewsList from './components/NewsList';
- 
+import Categories from './components/Categories';
+
 const App = () => {
   const [data, setData] = useState(null);
   //original version
@@ -14,25 +15,16 @@ const App = () => {
   */
 
   //async?
-  const onClick = async () => {
-    try{
-      var res = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=ab27d8e8234342ee84c8c97aae124ed0');
-      setData(res.data);
-      console.log(res.data);
-    }
-    catch(e){
-      console.log(e);
-    }
-  }
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
   return (
     <div>
       <div>
-        <button onClick={onClick}>불러오기</button>
-        <NewsList />
+        <Categories category={category} onSelect={onSelect} />
+        <NewsList category={category} />
       </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
     </div>
   );
 };
- 
+
 export default App;
